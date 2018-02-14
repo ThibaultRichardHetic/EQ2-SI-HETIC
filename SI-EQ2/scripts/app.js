5e3285c93b449201b151c3f25365c787aa510b35
AxelThomas <32869796+AxelThomas@users.noreply.github.com>
2018-02-14 20:38:19 +0100

    ajout du main v1

--- SI-EQ2/scripts/app.js
+++ SI-EQ2/scripts/app.js
@@ -1,1 +1,40 @@
-console.log("hello")
+$(document).ready(function(){
+  $('.slickSlider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  autoplay: true,
+  autoplaySpeed:3000 ,
+  arrows: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 720,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+  ]
+})
+$('.slickSliderIncoutournable').slick({
+  infinite: true,
+  dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed:2000,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+});
