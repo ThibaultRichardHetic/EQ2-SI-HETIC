68d5bf9651443a6ed91b48d17f201352f6356238
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-02-15 17:40:32 +0100

    compilation dossier

--- SI-EQ2/scripts/app.js
+++ SI-EQ2/scripts/app.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint:1350,
+      breakpoint:1200,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -18,7 +18,7 @@ $(document).ready(function(){
       }
     },
     {
-      breakpoint: 1050,
+      breakpoint: 960,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
@@ -42,4 +42,34 @@ $('.slickSliderIncoutournable').slick({
   slidesToShow: 1,
   slidesToScroll: 1
 });
+});
+
+//début du hamburger
+
+var content = document.querySelector('#hamburger-content');
+var sidebarBody = document.querySelector('#hamburger-sidebar-body');
+var button = document.querySelector('#hamburger-button');
+var overlay = document.querySelector('#hamburger-overlay');
+var activatedClass = 'hamburger-activated';
+
+sidebarBody.innerHTML = content.innerHTML;
+
+button.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	this.parentNode.classList.add(activatedClass);
+});
+
+button.addEventListener('keydown', function(e) {
+	if (this.parentNode.classList.contains(activatedClass))
+	{
+		if (e.repeat === false && e.which === 27)
+			this.parentNode.classList.remove(activatedClass);
+	}
+});
+
+overlay.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	this.parentNode.classList.remove(activatedClass);
 });
