fa30c4138602526355400966ac1eee2d35d499ed
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-02-15 01:34:03 +0100

    hamburger
    bientot la fin

--- SI-EQ2/scripts/app.js
+++ SI-EQ2/scripts/app.js
@@ -43,3 +43,33 @@ $('.slickSliderIncoutournable').slick({
   slidesToScroll: 1
 });
 });
+
+//début du hamburger
+
+var content = document.querySelector('#hamburger-content');
+var sidebarBody = document.querySelector('#hamburger-sidebar-body');
+var button = document.querySelector('#hamburger-button');
+var overlay = document.querySelector('#hamburger-overlay');
+var activatedClass = 'hamburger-activated';
+
+sidebarBody.innerHTML = content.innerHTML;
+
+button.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	this.parentNode.classList.add(activatedClass);
+});
+
+button.addEventListener('keydown', function(e) {
+	if (this.parentNode.classList.contains(activatedClass))
+	{
+		if (e.repeat === false && e.which === 27)
+			this.parentNode.classList.remove(activatedClass);
+	}
+});
+
+overlay.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	this.parentNode.classList.remove(activatedClass);
+});
